06cc36bd60ad577bf08734a7e057c13df0a8f807
Tomer Rosenfeld <mail@tomerrosenfeld.com>
2026-02-03 12:08:41 +0200

    add --help and --version flags

--- bin/dashmon.dart
+++ bin/dashmon.dart
@@ -1,6 +1,38 @@
 import 'package:dashmon/dashmon.dart';
 
+const version = '0.0.3';
+
+const helpText = '''
+Dashmon - Auto hot reload for Flutter applications
+
+Usage: dashmon [options] [flutter-run-args]
+       dashmon attach [options] [flutter-attach-args]
+
+Options:
+  -h, --help       Show this help message
+  -v, --version    Show version number
+  --fvm            Use FVM (Flutter Version Manager)
+
+Examples:
+  dashmon                    Run with auto hot reload
+  dashmon -d emulator-5555   Run on specific device
+  dashmon --fvm              Run using FVM
+  dashmon attach             Attach to running app
+
+All other arguments are passed directly to flutter run/attach.
+''';
+
 void main(List<String> args) {
+  if (args.contains('-h') || args.contains('--help')) {
+    print(helpText);
+    return;
+  }
+
+  if (args.contains('-v') || args.contains('--version')) {
+    print('dashmon $version');
+    return;
+  }
+
   print('Starting Dashmon...');
   final dashmon = Dashmon(args);
   dashmon.start();
